69be40c9f5269b29cd6e59206321316b3b84cf49
Dmitriy Sinitskiy <dmitriy.sinitskiy@quantori.com>
2024-05-14 15:09:40 +0300

    upgrade

--- app.py
+++ app.py
@@ -1,26 +1,90 @@
-import psycopg2
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse, abort
 
 from config import Config
+from db_utils import get_db_connection, prepare_tables
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+api = Api(app)
 
 
-def get_db_connection():
-    return psycopg2.connect(app.config['DATABASE_URL'])
+class ListUsers(Resource):
+
+    def get(self):
+        users = self._get_users_from_db()
+        return users, 200
+
+    def _get_users_from_db(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = (
+            "SELECT user_id, first_name, last_name, to_char(date_of_birth, 'YYYY-MM-DD'), gender, interests, city, profile_page FROM users")  # NoQA
+        cursor.execute(query)
+        result = cursor.fetchall()
+        conn.close()
+
+        users = []
+        for row in result:
+            user_data = {
+                'id': row[0],
+                'first_name': row[1],
+                'last_name': row[2],
+                'date_of_birth': row[3],
+                'gender': row[4],
+                'interests': row[5],
+                'city': row[6],
+                'profile_page': row[7],
+            }
+            users.append(user_data)
+
+        return users
 
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT NOW()')
-    result = cursor.fetchall()
-    conn.close()
-    return str(result)
+class GetUser(Resource):
+
+    def get(self, user_id):
+        user = self._get_user_by_id(user_id)
+        if user is None:
+            abort(404, message="User {} doesn't exist".format(user_id))
+        return user, 200
+
+    def _get_user_by_id(self, user_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = (
+            "SELECT user_id, first_name, last_name, to_char(date_of_birth, 'YYYY-MM-DD'), gender, interests, city, profile_page " # NoQA
+            "FROM users WHERE user_id = %s"
+        )  # NoQA
+        cursor.execute(query, (user_id,))
+        result = cursor.fetchall()
+        conn.close()
+
+        if result:
+            for row in result:
+                user_data = {
+                    'id': row[0],
+                    'first_name': row[1],
+                    'last_name': row[2],
+                    'date_of_birth': row[3],
+                    'gender': row[4],
+                    'interests': row[5],
+                    'city': row[6],
+                    'profile_page': row[7],
+                }
+                return user_data
+        else:
+            return None
+
+
+
+
+
+api.add_resource(ListUsers, '/users')
+api.add_resource(GetUser, '/users/<int:user_id>')
 
 
 if __name__ == '__main__':
+    prepare_tables()
     app.run(debug=True)
---
+++ db_utils.py
@@ -0,0 +1,51 @@
+import enum
+
+import psycopg2
+
+from config import Config
+
+
+def get_db_connection():
+    return psycopg2.connect(Config.DATABASE_URL)
+
+
+class UserFieldNames(enum.Enum):
+    id = "id"
+    first_name = "first_name"
+    last_name = "last_name"
+    date_of_birth = "date_of_birth"
+    gender = "gender"
+    interests = "interests"
+    city = "city"
+    profile_page = "profile_page"
+    password = "password"
+
+
+user_table_create_statement = """
+CREATE TABLE IF NOT EXISTS Users (
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    interests TEXT[],
+    city VARCHAR(100),
+    profile_page VARCHAR(255),
+    password VARCHAR(100)
+);
+"""
+
+# user_table_insert_statement = """
+# INSERT INTO Users (first_name, last_name, date_of_birth, gender, interests, city, profile_page)
+# VALUES ('Name', 'Lastname', '1990-01-01', 'Male', '{"Interest_1", "Interest_2"}', 'The City', 'http://link_to_profile');
+# """
+
+
+def prepare_tables():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(user_table_create_statement)
+
+    conn.commit()
+    conn.close()
